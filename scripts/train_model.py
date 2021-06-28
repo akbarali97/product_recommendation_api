49174dec737af376e5bedbf71f2ed6c91a2886bd
Akbar Ali <akkuakbar97@gmail.com>
2021-06-27 19:13:08 -0700

    june 27, 2

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,17 +1,25 @@
+#setting up all the essentials
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg
 
-def train_model():
 
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(400, 400, 3)),
-        keras.layers.Conv2D(32,kernel_size=3,strides=(1,1),activation='relu', padding='same'),
-        keras.layers.Conv2D(32,kernel_size=3,strides=(2,2),activation='relu', padding='same'),
-        keras.layers.Conv2D(64,kernel_size=3,strides=(1,1),activation='relu', padding='same'),
-        keras.layers.Conv2D(64,kernel_size=3,strides=(2,2),activation='relu', padding='same'),
-        keras.layers.Conv2D(128,kernel_size=3,strides=(1,1),activation='relu', padding='same'),
-        keras.layers.Conv2D(128,kernel_size=3,strides=(2,2),activation='relu', padding='same'),
-        keras.layers.Conv2D(256,kernel_size=3,strides=(1,1),activation='relu', padding='same'),
-        keras.layers.Conv2D(256,kernel_size=3,strides=(2,2),activation='relu', padding='same'),
-        keras.layers.GlobalAveragePooling2D(),
-        keras.layers.Dense(128, activation=tf.nn.relu),
-        keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
+#machine learning libraries 
+from keras.models import Sequential, Model
+from keras_preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras import regularizers, optimizers
+from keras.applications.mobilenet_v2 import MobileNetV2
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+
+import os # accessing directory structure
+
+
+#import and setup training dataset
+TRAINING_DATASET_PATH = "../dataset/"
+training_data = pd.read_csv(TRAINING_DATASET_PATH + "styles.csv", error_bad_lines=False)
+training_data['image'] = training_data.apply(lambda row: str(row['id']) + ".jpg", axis=1)
+training_data = training_data.sample(frac=1).reset_index(drop=True)
+training_data.head(10)
